10c93d251e8910536a96b1d2c99f2b4c93a5e008
EldarRach7 <rachmilov88@gmail.com>
2026-05-23 18:29:32 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+print("hello world")
+name = input("what is your name ?")
+age = int(input("what is your age?"))
+print ("hello " + name + "! "+ "in 10 years you will be " + str(age + 10) + " years old")
+for i in range(1,11):
+    print(i)
+
+print("loop has ended")
+    
+    
